9d6774ba684e96c4460e666f239e2f3351805f80
DionysGerin <diogerin@gmail.com>
2016-03-23 11:18:40 +0100

    files moved try 2

---
+++ Dominion/src/dominion/Game.java
@@ -0,0 +1,7 @@
+package dominion;
+
+public class Game 
+{
+	private Player[] players;
+	private ActionCards[] shop;
+}
---
+++ Dominion/src/dominion/VictoryCards.java
@@ -0,0 +1,14 @@
+package dominion;
+
+public class VictoryCards 
+{
+	private String name;
+	private int cost;
+	private int value;
+	
+	public VictoryCards(String name, int cost)
+	{
+		this.name = name;
+		this.cost = cost;
+	}
+}
