1d57c368227ba9d5607245a34809c8874aceaaf1
DionysGerin <diogerin@gmail.com>
2016-03-24 13:43:07 +0100

    nog turn wat aangepast

--- DominionEngine/src/dominion/Turn.java
+++ DominionEngine/src/dominion/Turn.java
@@ -5,49 +5,61 @@ public class Turn
 	private int actions;
 	private int buys;
         private boolean isOver;
+        private int money;
 	
 	public Turn()
 	{
-		actions = 1;
-		buys = 1;
-                isOver = false;
+            actions = 1;
+            buys = 1;
+            isOver = false;
+            money = 0;
 	}
 	
 	public int getActions()
 	{
-		return actions;
+            return actions;
 	}
 	
 	public int getBuys()
 	{
-		return buys;
+            return buys;
 	}
 	
 	public void reduceActions()
 	{
-		actions--;
+            actions--;
 	}
 	
 	public void reduceBuys()
 	{
-		buys--;
+            buys--;
 	}
 	
 	public void addActions(int amount)
 	{
-		actions += amount;
+            actions += amount;
 	}
 	
 	public void addBuys(int amount)
 	{
-		buys += amount;
+            buys += amount;
 	}
+        
+        public void addMoney(int amount)
+        {
+            money += amount;
+        }
+        
+        public void reduceMoney(int amount)
+        {
+            money -= amount;
+        }
 	
 	public void endTurn()
 	{
-		actions = 0;
-		buys = 0;
-                isOver = true;
+            actions = 0;
+            buys = 0;
+            isOver = true;
 	}
         
         public boolean isOver()
