383052aa6d601f978667c383608a22b500eadbe8
DionysGerin <diogerin@gmail.com>
2016-03-25 10:48:49 +0100

    iets

--- DominionEngine/src/dominion/Maintester.java
+++ DominionEngine/src/dominion/Maintester.java
@@ -2,7 +2,8 @@ package dominion;
 
 import java.util.ArrayList;
 
-
+// Waarom gebruiken we eigenlijk inheritants ? is er daar een reden voor dionys?? kunnen we niet beter gewoon 3 klassen maken voor de soorten kaarten ?
+//ander los als de f*** shit op da nie werkt door inheritance 
 public class Maintester 
 {
     public static void main(String[] args)
